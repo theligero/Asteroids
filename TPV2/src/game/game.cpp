c5bc825cd6915e823377a5f6796f81994dd361fd
theligero <iligero@ucm.es>
2023-02-21 18:34:40 +0100

    una pequeña adición
    cambiadas los tamaños de ancho y alto de la pantalla por constantes ancho y alto al inicio del game.cpp (cambiar más adelante al game.h)

--- TPV2/src/game/game.cpp
+++ TPV2/src/game/game.cpp
@@ -7,11 +7,14 @@
 #include "../components/DeAcceleration.h"
 #include "../components/DisableOnExit.h"
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
 Game::Game()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	window = SDL_CreateWindow("Asteroids v1.0", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+		SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	for (int i = 0; i < NUM_TEXTURES; ++i) {
@@ -35,12 +38,12 @@ void Game::run()
 	
 	auto fighter = man.addEntity();
 	auto asteroid = man.addEntity();
-	fighter->addComponent<Transform>(TRANSFORM, Vector2D(125, 125), Vector2D(- 0.0125, - 0.0125), 85, 77, 0);
+	fighter->addComponent<Transform>(TRANSFORM, Vector2D(125, 125), Vector2D(- 0.0125, - 0.0125), 35, 30, 0);
 	auto imageFighter = fighter->addComponent<Image>(IMAGE, arrayTex[FIGHTER].get());
-	fighter->addComponent<ShowAtOpposideSide>(SHOW_AT_OPPOSIDE_SIDE, 800, 600);
+	fighter->addComponent<ShowAtOpposideSide>(SHOW_AT_OPPOSIDE_SIDE, WINDOW_WIDTH, WINDOW_HEIGHT);
 	fighter->addComponent<FighterCtrl>(FIGHTER_CTRL);
 	fighter->addComponent<DeAcceleration>(DEACCELERATION);
-	// fighter->addComponent<DisableOnExit>(DISABLE_ON_EXIT, 800, 600);
+	// fighter->addComponent<DisableOnExit>(DISABLE_ON_EXIT, WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 void Game::render()
